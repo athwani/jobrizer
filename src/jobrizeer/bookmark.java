49fc5d6cc9d8a07fc644a40fbc704fe690c449f4
Sagar <athwani.sagar1@gmail.com>
2018-05-09 11:10:32 +0400

    update purchase

--- src/jobrizeer/bookmark.java
+++ src/jobrizeer/bookmark.java
@@ -2,4 +2,8 @@ package jobrizeer;
 
 public class bookmark {
 
+	public void bkmark(WebDriver driver) throws Exception{
+		
+		
+	}
 }
--- src/jobrizeer/entryclass.java
+++ src/jobrizeer/entryclass.java
@@ -15,19 +15,22 @@ public class entryclass  {
 		       driver.manage().window().maximize();
 		// Open Jobrizer
 					driver.get("http://www.jobrizzer.iosbuild.net");
-	    //Click on Employer Link	
+	    //Click on Employer Link		
 			driver.findElement(By.linkText("Employer")).click();
 			Thread.sleep(2000);	
 				
 			//Login Page 
 			driver.findElement(By.linkText("Login")).click();
-			Thread.sleep(1000);
-			newsfeed N1 = new newsfeed();
+	           Thread.sleep(1000);
+			
+	       
+	   	   	purchase P2 = new purchase();
+	   	       P2.purchas1(driver);
+	           
+	           newsfeed N1 = new newsfeed();
 		   	N1.newfeed(driver);
 		   	
-			// Open Newsfeed page
-		   	driver.findElement(By.xpath("//li[contains(@class='active')]")).click();
-		   	Thread.sleep(1000);
+			
 		   	
 	   	 login L1 = new login();
               L1.VUIP(driver);
@@ -44,17 +47,21 @@ public class entryclass  {
 	   //Open Register Page
 			driver.findElement(By.linkText("Signup")).click();
 			Thread.sleep(1000);
-		
-	   	    Register R1 = new Register();
+		 Register R1 = new Register();
 	   	   
 
 	   // Open bookmark page
 	   	driver.findElement(By.partialLinkText("Bookmark")).click();
-        Thread.sleep(2000); 
-    
-	   	bookmark B1 = new bookmark();	
+             Thread.sleep(2000); 
+         bookmark B1 = new bookmark();	
 	 
 	
 	   	store_string S1 = new store_string();
-      }
+    
+	   	//Open Purchase page 
+	   	driver.findElement(By.xpath("//a[@data-target='.navbar-collapse.in'][contains(text(),'Purchase')]"));
+             Thread.sleep(2000);
+	   	purchase P1 = new purchase();
+	       P1.purchas1(driver);
+	}
 }
